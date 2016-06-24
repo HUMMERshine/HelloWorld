a77f7832f2cebbd93543e4d9ab560894f8201b80
lst <a_lst@163.com>
2016-06-24 09:05:59 +0800

    dddd

--- src/priv/lst/test/TestBranch.java
+++ src/priv/lst/test/TestBranch.java
@@ -2,9 +2,10 @@ package priv.lst.test;
 
 public class TestBranch
 {
-public static void main(String[] args)
-{
-    String str1= null;
-    int e=0;
-}
+    public static void main(String[] args)
+    {
+        String str1 = null;
+        int e = 0;
+        
+    }
 }
