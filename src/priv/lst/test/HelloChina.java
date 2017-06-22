ebfd6e53df19269262faabb29573ee98c95d0eb9
lst-bytedance <lst-bytedance@lst.local>
2017-06-22 18:57:56 +0800

    test amend and amend2
    Change-Id: I0cb253831edb439204eb239044620ed4846245b4

--- src/priv/lst/test/HelloChina.java
+++ src/priv/lst/test/HelloChina.java
@@ -3,7 +3,9 @@ package priv.lst.test;
 public class HelloChina
 {
 	public static void main(String [] args){
-		System.out.println("Hello world!")
+		System.out.println("git");
+		System.out.println("git amend");
+		System.out.println("Hello world!");
 	}
 
 }
